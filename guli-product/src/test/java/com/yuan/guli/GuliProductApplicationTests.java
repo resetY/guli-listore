2a294e5ff0fa5a444b0a148e1395585e6fe014dd
lzy <1716827691@qq.com>
2022-03-07 09:32:10 +0800

    项目初始结构创建

---
+++ guli-product/src/test/java/com/yuan/guli/GuliProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.guli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
